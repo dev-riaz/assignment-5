3ed64f714d523acbd0f67ecbdc8ea734628807b9
riaz113 <riazmahamud113@gmail.com>
2026-04-04 17:51:07 +0600

    finished the project

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,10 @@
-
 const cardContainer = document.getElementById("cardContainer");
 const OpenCardContainer = document.getElementById("Open-card-container");
 const closedCardContainer = document.getElementById('closed-card-container');
+const modalContainer = document.getElementById('my_modal_1');
+const spinner = document.getElementById('loadSpinner');
 
+let allData = [];
 const createElement = (arr) => {
     const htmlElements = arr.map(
         (el) =>
@@ -23,24 +25,30 @@ const createElement = (arr) => {
 };
 
 let counts = {
-    all : 0,
-    open : 0,
-    closed : 0
+    all: 0,
+    open: 0,
+    closed: 0
 }
 
 async function loadCard() {
     cardContainer.innerHTML = "";
     OpenCardContainer.innerHTML = "";
     closedCardContainer.innerHTML = "";
+    modalContainer.innerHTML = "";
+
+    spinner.classList.remove('hidden')
     const res = await fetch(
         "https://phi-lab-server.vercel.app/api/v1/lab/issues"
     );
     const data = await res.json();
+    allData = data.data;
+
     showCount(data.data.length)
 
+    spinner.classList.add('hidden')
     const openIssues = data.data.filter(item => item.status === 'open');
     const closedIssues = data.data.filter(item => item.status === 'closed');
-    
+
     counts.all = data.data.length;
     counts.open = openIssues.length;
     counts.closed = closedIssues.length;
@@ -48,7 +56,7 @@ async function loadCard() {
 
     const card = (item) => {
         return `
-        <div class="shadow-sm bg-white rounded-xl h-full flex flex-col justify-between border-t-4 ${item.status === "open" ? "border-t-[#00A96E]" : "border-t-purple-500"}">
+        <div onclick="handleModal(${item.id})"class="shadow-sm bg-white rounded-xl h-full flex flex-col justify-between border-t-4 ${item.status === "open" ? "border-t-[#00A96E]" : "border-t-purple-500"}">
 
                         <div class="p-4 space-y-3">
                             <div class="flex justify-between items-center">
@@ -73,26 +81,26 @@ async function loadCard() {
         ` ;
 
     }
-    data.data.forEach(item=> {
+    data.data.forEach(item => {
         const div = document.createElement("div");
         div.innerHTML = card(item);
         cardContainer.appendChild(div);
-        
+
     });
     openIssues.forEach(item => {
         const div = document.createElement("div");
         div.innerHTML = card(item);
         OpenCardContainer.appendChild(div);
-        
-        
+
+
     });
     closedIssues.forEach(item => {
         const div = document.createElement("div");
         div.innerHTML = card(item);
         closedCardContainer.appendChild(div);
-        
+
     });
-    
+
 }
 
 
@@ -131,4 +139,115 @@ const showIssue = document.querySelectorAll('.showBtn').forEach(btn => {
 
     })
 })
-loadCard()
+
+
+const handleModal = (id) => {
+    const singleData = allData.find(item => item.id == id);
+
+    loadModal(singleData);
+
+    my_modal_1.showModal();
+};
+const loadModal = (el) => {
+    if (!el) return;
+
+    modalContainer.innerHTML = `
+    <div class="modal-box space-y-3">
+                        <h1 class="text-[#1F2937] text-xl font-semibold">${el.title}</h1>
+                        <div class="flex items-center gap-6">
+                            <div class="badge badge-sm ${el.status === "open" ? "bg-[#00A96E] text-white" : "bg-purple-500"}">${el.status}</div>
+                            <ul class="flex items-center gap-6">
+                                <li class="list-disc text-[#64748B] font-semibold text-[12px]">Opened by ${el.author}</li>
+                                <li class="list-disc text-[#64748B] font-semibold text-[12px]">${el.createdAt}</li>
+                            </ul>
+                        </div>
+                        <div class="">
+                            <div>${createElement(el.labels)}</div>
+                        </div>
+                        <p class="text-[#64748B] text-[14px]">${el.description}</p>
+                        <div class="bg-[#F8FAFC] w-full flex gap-30 p-3">
+                            <div class="flex flex-col">
+                                <span class="text-[#64847B] font-semibold text-[14px]">Assignee:</span>
+                                <span class="text-[#64847B] font-medium text-[12px]">${el.assignee}</span>
+                            </div>
+                            <div class="flex flex-col">
+                                <span class="text-[#64847B] font-semibold text-[14px]">Priority:</span>
+                                <div class="badge ${el.priority === 'high' ? 'bg-[#FEECEC] text-[#EF4444]' : el.priority === "medium" ? 'bg-[#FFF6D1] text-[#F59E0B]' : el.priority === "low" ? 'bg-[#EEEFF2] text-[#9CA3AF]' : ""}">${el.priority}</div>
+                            </div>
+                        </div>
+                        <div class="modal-action">
+                            <form method="dialog">
+                                <button class="outline-none btn btn-primary">Close</button>
+                            </form>
+                        </div>
+                    </div>
+    `;
+};
+
+
+document.getElementById('search-btn').addEventListener('click', () => {
+    const input = document.getElementById('input-search');
+
+    const searchValue = input.value.trim();
+
+    if (!searchValue) {
+        loadCard();
+        return;
+    }
+    async function loadSearch() {
+        cardContainer.innerHTML = "";
+        OpenCardContainer.innerHTML = "";
+        closedCardContainer.innerHTML = "";
+
+        const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
+
+        const data = await res.json()
+
+        const issues = data.data || [];
+
+        showCount(issues.length);
+        counts.all = issues.length;
+        counts.open = issues.filter(i => i.status === 'open').length;
+        counts.closed = issues.filter(i => i.status === 'closed').length;
+
+        issues.forEach(element => {
+            const div = document.createElement("div");
+            div.innerHTML = `
+        <div onclick="handleModal(${element.id})"class="shadow-sm bg-white rounded-xl h-full flex flex-col justify-between border-t-4 ${element.status === "open" ? "border-t-[#00A96E]" : "border-t-purple-500"}">
+
+                        <div class="p-4 space-y-3">
+                            <div class="flex justify-between items-center">
+                                <img src="${element.status === 'open' ? "./assets/Open-Status.png" : element.status === 'closed' ? "./assets/Closed- Status .png" : ""}" alt="">
+                                <div class="badge badge-md px-6 rounded-full ${element.priority === 'high' ? 'bg-[#FEECEC] text-[#EF4444]' : element.priority === "medium" ? 'bg-[#FFF6D1] text-[#F59E0B]' : element.priority === "low" ? 'bg-[#EEEFF2] text-[#9CA3AF]' : ""}">${element.priority}</div>
+                            </div>
+                            <div class="space-y-3">
+                                <h1 class="text-[#64748B] text-[14px] font-semibold line-clamp-1">${element.title}</h1>
+                                <p class="text-[#64748B] text-[12px] line-clamp-2">${element.description}</p>
+                                <div class="">
+                                    <div class="rounded-full">${createElement(element.labels)}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="border text-base-300">
+                        <div class="p-4 space-y-1">
+                            <p class="text-[#64748B] text-[12px]">#1 by${element.author}-${element.createdAt}</p>
+                            <p class="text-[#64748B] text-[12px]">${element.assignee}-${element.updatedAt}</p>
+                        </div>
+        
+        </div>  
+    `;
+            cardContainer.appendChild(div);
+            if (element.status === "open") {
+                OpenCardContainer.appendChild(div.cloneNode(true));
+            } else if (element.status === "closed") {
+                closedCardContainer.appendChild(div.cloneNode(true));
+            }
+
+        });
+
+
+    }
+    loadSearch()
+
+})
+loadCard();
